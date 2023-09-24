3fe0e3aa5fff2f7ec5b9246e25d940ab89d0352a
Nabil Hellal <nabil-13130@hotmail.fr>
2023-09-24 14:15:52 +0200

    Projet previsualisateur markdown terminé

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// Librairies
+import { marked } from 'marked';
+
 function App() {
+// State
+  const [message, setMessage] = useState("")
+
+// Methode
+  const valueOfTextarea = (e) =>{
+    setMessage(e.target.value)
+  }
+
+  const getMarkdown = () => {
+    let rawMarkup = marked.parse(message);
+    return { __html: rawMarkup }
+  }
+
+// JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea onChange={ valueOfTextarea }></textarea>
+      <div className='block' dangerouslySetInnerHTML={getMarkdown()}></div>
     </div>
   );
 }
